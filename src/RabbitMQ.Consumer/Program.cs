140df44e72c36efd0a13f0659385749852023ec6
Houston da Paz <houston.diego@gmail.com>
2023-11-01 09:27:01 -0300

    :tada: consumer project structure

---
+++ src/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,10 @@
+using RabbitMQ.Consumer;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
